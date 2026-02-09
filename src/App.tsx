3c17ce9e1d80ce8af601d7bfe903d4051962208f
Ahmet Can Buyukyilmaz <acb3@outlook.com>
2026-02-09 18:52:43 +0300

    added tooltips

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,18 @@ import "@/styles/index.css";
 import { CoinsList } from "@/pages/coins/views/coins-list.tsx";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { QueryClient } from "@tanstack/react-query";
+import { TooltipProvider } from "@/components/ui/tooltip.tsx";
 
 const queryClient = new QueryClient();
 
 function App() {
   return (
     <QueryClientProvider client={queryClient}>
-      <main className="h-screen w-screen">
-        <CoinsList />
-      </main>
+      <TooltipProvider>
+        <main className="h-screen w-screen">
+          <CoinsList />
+        </main>
+      </TooltipProvider>
     </QueryClientProvider>
   );
 }
