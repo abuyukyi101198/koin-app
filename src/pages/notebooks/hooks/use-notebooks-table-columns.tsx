4ca10ed59828e4b86ee84a037acf79c8fafb4b9f
Ahmet Can Buyukyilmaz <acb3@outlook.com>
2026-02-26 15:27:54 +0300

    use-notebooks-table-columns.tsx for the notebook-list DataTable

---
+++ src/pages/notebooks/hooks/use-notebooks-table-columns.tsx
@@ -0,0 +1,53 @@
+import { useMemo } from "react";
+
+import { ColumnDef } from "@tanstack/react-table";
+
+import { Notebook } from "@/query/types/notebooks.ts";
+
+export function useNotebooksTableColumns(): ColumnDef<Notebook>[] {
+  return useMemo(
+    () =>
+      [
+        {
+          id: "title",
+          accessorKey: "title",
+          enableSorting: false,
+          meta: {
+            size: 60,
+          },
+          cell: ({
+            row: {
+              original: { title, description },
+            },
+          }) => {
+            const displayTitle =
+              title.length > 60
+                ? `${title.substring(0, 60).trimEnd()}...`
+                : title;
+
+            const displayDescription =
+              description && description.length > 60
+                ? `${description.substring(0, 60).trimEnd()}...`
+                : description;
+
+            return (
+              <div className="w-full flex flex-col justify-between">
+                <span title={title.length > 60 ? title : undefined}>
+                  {displayTitle}
+                </span>
+                {description && (
+                  <span
+                    className="text-xs italic text-muted-foreground leading-5 grow"
+                    title={description.length > 60 ? description : undefined}
+                  >
+                    {displayDescription}
+                  </span>
+                )}
+              </div>
+            );
+          },
+        },
+      ] as ColumnDef<Notebook>[],
+    []
+  );
+}
